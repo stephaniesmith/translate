176f41970e90bf4937d9250ae27c0725d027d28d
Stephanie Smith <stephsmithcode@gmail.com>
2019-03-12 20:44:15 -0700

    lang comp

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import UserCreate from './UserCreate';
+import LanguageSelector from './LanguageSelector';
 import LanguageContext from '../contexts/LanguageContext';
 import ColorContext from '../contexts/ColorContext';
 
@@ -13,11 +14,7 @@ class App extends Component {
   render() {
     return (
       <div className="ui container">
-        <div>
-          Select a language: 
-          <i className="flag us" onClick={() => this.onLanguageChange('english')}/>
-          <i className="flag nl" onClick={() => this.onLanguageChange('dutch')}/>
-        </div>
+        <LanguageSelector onLanguageChange={this.onLanguageChange}/>
         <LanguageContext.Provider value={this.state.language}>
           <ColorContext.Provider value="red">
             <UserCreate/>
--- src/components/LanguageSelector.js
+++ src/components/LanguageSelector.js
@@ -3,10 +3,12 @@ import React, { Component } from 'react';
 export class LanguageSelector extends Component {
 
   render() {
-
+    const { onLanguageChange } = this.props;
     return (
       <div>
-
+        Select a language: 
+        <i className="flag us" onClick={() => onLanguageChange('english')}/>
+        <i className="flag nl" onClick={() => onLanguageChange('dutch')}/>
       </div>
     );
   }
